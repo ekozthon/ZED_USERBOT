edf20101ac87ce507528f60582a5349bd61848bd
ekozthon <92107110+ekozthon@users.noreply.github.com>
2021-10-10 18:41:50 +0300

    Update and rename userbot/plugins/ازرار.py to مستخدم الروبوت/الإضافات/ازرار.py

--- مستخدم الروبوت/الإضافات/ازرار.py
+++ مستخدم الروبوت/الإضافات/ازرار.py
@@ -1,7 +1,7 @@
 #    Copyright (C) 2020  Teamthon
 # button post makker for ZedThon thanks to uniborg for the base
 # by @sandy1709 (@mrconfused)
-# edit @ZedThon (zed)
+# edit @DARKTHON (DARKEN)
 
 import os
 import re
